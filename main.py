9108715fc96a0b7ed312b44f676efc71e9c578af
tanymanera <gaetanomanera@outlook.it>
2023-01-15 15:43:01 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.header('st.write')
+
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+diz = {
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     }
+st.write(diz)
+
+df = pd.DataFrame(diz, index=[ 'A', 'B', 'C', 'D'])
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+chart = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(chart)
